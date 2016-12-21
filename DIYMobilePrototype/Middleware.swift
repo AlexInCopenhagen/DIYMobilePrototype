32d00705e650579355317a20a6e6f015bcb71ad5
Alex Saberdine <alexsaberdine@alexs-macbook-pro.local>
2016-12-21 12:08:50 +0100

    Initial Project File Structure setup

---
+++ DIYMobilePrototype/Middleware.swift
@@ -0,0 +1,9 @@
+//
+//  Middleware.swift
+//  DIYMobilePrototype
+//
+//  Created by Alex Saberdine on 21/12/2016.
+//  Copyright © 2016 Alex Saberdine. All rights reserved.
+//
+
+import Foundation
---
+++ DIYMobilePrototype/Store.swift
@@ -0,0 +1,9 @@
+//
+//  Store.swift
+//  DIYMobilePrototype
+//
+//  Created by Alex Saberdine on 21/12/2016.
+//  Copyright © 2016 Alex Saberdine. All rights reserved.
+//
+
+import Foundation
