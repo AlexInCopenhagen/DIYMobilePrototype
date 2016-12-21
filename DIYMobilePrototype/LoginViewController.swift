bc80eb506adb9a846aa2809ff296c719522f7f5d
Alex Saberdine <alexsaberdine@Alexs-MacBook-Pro.local>
2016-12-21 15:46:57 +0100

    Added initial workflow

---
+++ DIYMobilePrototype/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DIYMobilePrototype
+//
+//  Created by Alex Saberdine on 21/12/2016.
+//  Copyright © 2016 Alex Saberdine. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .lightGray
+        
+    }
+
+
+
+
+}
+
